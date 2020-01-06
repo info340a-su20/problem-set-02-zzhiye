4edf2851ec06bb289423d77f8c7d8db377c5aafd
Joel Ross <joelross@uw.edu>
2020-01-05 22:27:12 -0800

    Rewrite problem-a

--- problem-a/README.md
+++ problem-a/README.md
@@ -1,37 +1,54 @@
 # Problem A
 
-In this exercise, you'll practice working with HTML, in particularly modifying content to be more **accessible** by using semantic elements.
+In this exercise, you'll practice working with HTML, in particularly modifying content to be more **accessible** by using semantic elements. You'll also learn to structure HTML forms!
 
 To complete the exercise, modify the included `index.html` file (which displays a simple blog) based on the below instructions. You will **not** need to modify the CSS (which has been included to make the page look at little nicer, _AND_ to give some visual hints when you're on the right track!).
 
-In particular, you should make the following changes:
+In particular, you will need to make the following changes:
 
-1. Ensure that heading elements (e.g., `<h1>`, `<h2>`) are used appropriately: they should be **meaningful** (for text that are actually headings, not just for styling) and **hierarchical** (in order, `h2` following `h1`).
+1. Web pages should be organized using _semantic sectioning_ elements, which makes them easier to navigate for screen readers by providing ARIA navigation landmarks. Note that adding these elements will also apply some styling (so you can visually check that it's right!). Add the following elements:
 
-    - You should use the `time-posted` CSS class defined in `css/style.css` to apply styling to the "time posted" elements, instead of inappropriately using headings.
+	- Both the "Page Title" elements should be in a single `<header>` element.
 
-2. Add _semantic sectioning_ elements to appropriately organize the document. These will also provide ARIA navigation landmarks. Note that adding these elements will also apply some styling (so you can visually check that it's right!)
+	- All of the "Image Post" and the "Comment Form" elements should be in a `<main>` element. The "Image Post" elements should then be in a `<section>` (inside of the `<main>`), while the "Comment Form" elements should be a second `<section>` in the `<main>`.
+	
+	- In addition, the first two elements of the "Image Post" ("The Cutest Puppy!" and "Posted last year") should be in a `<header>` element that is inside of that `<section>`&mdash;the header of that section!
 
-    - The blog title and subtitle elements should children of a `<header>` element.
+  - The "Author Info" elements should be in a single `<footer>` element.
 
-    - All of the blog posts should be children of a `<main>` element, with the full content of each post (including its title) in individual `<section>` elements.
+2. Heading elements (e.g., `<h1>`, `<h2>`) should be used appropriately. 
 
-3. Each blog post has an element indicating the time it was posted (e.g., "Posted today"). These times are _relative_ (e.g., "yesterday" not "June 13th"). Add [HTML Markup](https://css-tricks.com/time-element/) so that the time (e.g., the word "today") is machine readable for search engine optimization. Be sure to include attributes to give these times machine-readable dates&mdash;"today" should be today's data, "yesterday" should be yesterday's date, etc.
+	- Headings should be **meaningful**, used for actual headings and not for styling. Change the `<h6>` element so that it is instead a paragraph element with the class **`time-posted`**; that class will apply the provided CSS to style the element (rather than using the styling provided by the `<h6>` tag)
 
-    _Yes, you will have to look up a new HTML tag for this exercise! Follow the links!_
+	- Headings should also be **hierarchical** (in order, `h3` following `h2 ` without skipping levels). Change the `<h3>` so it is the appropriate level. Headings are used to define a "table of contents" for your page!
 
-4. The first blog post contains an image. Make sure the image has an `alt` tag so that is properly read by screen readers (the description is up to you). Additionally, make the paragraph below the image describing it into a proper [caption](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/figcaption).
+3. HTML elements can provide further annotations to help machines semantically understand them (for search engine optimization, etc). Wrap the words "last year" (in "Posted last year") in a [`<time>` element](https://css-tricks.com/time-element/). Be sure to include an attribute to indicate that the phrase "last year" in this case refers to the date "Sep 01, 2019". 
 
-  - The caption describes the image source. Include [markup](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/cite) to make this semantically explicit. Note that the _entire_ caption (including the picture name, photographer, and link) is a citation!
+4. Make sure the image has an `alt` attribute so that is properly read by screen readers (the description is up to you&mdash;remember that `alt` should finish the sentence "an image of...").
 
-5. The second blog post talks about writing code, but could use some help to make it look and read correctly:
+5. The text currently include a number of `<i>` elements. This element has no actual semantic meaning, so you should fix that!
 
-  - Add [markup](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/abbr) to the abbreviations "HTML" and "CSS" (the first time they are used), explaining what they stand for. (This will let you "mouse over" the text to see the expansion in some browsers).
+	- Where the `<i>` is used for emphasis, replace it with an `<em>` element.
+	- Where the `<i>` is used to mark a citation, replace it with a `<cite>` element.
 
-  - The post tries to show some HTML code, but it doesn't render correctly. Use [HTML Character Entities](https://developer.mozilla.org/en-US/docs/Glossary/Entity) to render the HTML tag as text. Additionally, add [markup](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/code) to indicate that this is computer _code_.
+6. The "Comment Form" is an example of a **form**&mdash;a set of elements that collect user input for processing. Users can type values into the `<input>` boxes. But you'll need to make some modifications to ensure that this form is accessible and functional:
 
-  - The CSS rule components (_"Selectors"_ and _"Properties"_) are listed with descriptions... so should more appropriately be structured as a [description list](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Define_terms_with_HTML#How_to_build_a_description_list) (`dl`). Change the `ul` to `dl`. Note that each "item" in a description list is actually two elements: a _term_ (`dt`, e.g. for "Selectors") and a _description_ (`dd`, e.g., for the description).
+	- An `<input>` element can have a `type` attribute which indicates what kind of input it is. Give the "email" input a `type` of `"email"` and the "password" input a `type` of `"password"`. The later has the most noticable effect, hiding the password being typed.
 
-6. Contact information (such as in the footer at the bottom of the page) should be inside of an [`<address>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/address) element. Note that addresses that are _not_ contact information for the current document or article require no special markup.
+	- Each input element will need an attribute `name` that will let form processing identify which field the user input is for. Give the "email" input a `name` of `"email"`, the "password" input a `name` of `"password"`, and the "comment" input (the `<textarea>`) a `name` of `"comment"`. 
+	
+		Yes, this makes forms have a lot of redundancy. The `id` is used for referring to the element, the `type` indicates what it's used for, and the `name` is used for tracking the value of that input.
 
-  - Additionally, make sure that the [email](https://css-tricks.com/snippets/html/mailto-links/) and [telephone](https://css-tricks.com/the-current-state-of-telephone-links/) number are both links with proper URI protocols!
+	- The text describing what each `input` is for should be put in a `<label>` element (to indicate that it is a label for that input). Each `<label>` will need a `for` attribute whose value is the `id` of the `input` that label is associated with. When this is set up, you can click on the label and the cursor will jump to the appropriate input!
+
+		Add a `<label>` for _each_ of the 3 input: the two `<input>` elements and the `<textarea>`.
+	
+	- The "submit" button just has a picture of a pencil instead of a readable word. To make sure that image is understood by screen readers, give the icon (the `<i>` element) and `aria-label` attribute with a value of `"Submit"`. This is similar to adding an `alt` attribute, but works for elements that aren't `img`.
+
+7. Finally, the footer needs some additional markup to make its semantic meaning clear:
+
+	- The sentence containing contact information ("Contact me at...") should be inside of an [`<address>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/address) element instead of a `<p>`. Note that the `<address>` element is only for contact information, not for general postal addresses.
+
+	- Make sure that the [email](https://css-tricks.com/snippets/html/mailto-links/) and [telephone](https://css-tricks.com/the-current-state-of-telephone-links/) number are both links (`<a>`) with proper URI protocols!
+
+	- Replace the word "copyright" with an [HTML Character Entity](https://developer.mozilla.org/en-US/docs/Glossary/Entity) for the copyright symbol. This will ensure that the symbol renders no matter the language settings of the computer.
--- problem-a/problem.spec.js
+++ problem-a/problem.spec.js
@@ -12,6 +12,7 @@ describe('Source code is valid', () => {
   test('HTML validates without errors', async () => {
     const lintOpts = {
       'attr-bans':['align', 'background', 'bgcolor', 'border', 'frameborder', 'marginwidth', 'marginheight', 'scrolling', 'style', 'width', 'height'], //adding height, allow longdesc
+      'tag-bans':['style','b'], //<i> allowed for font-awesome
       'doctype-first':true,
       'doctype-html5':true,
       'html-req-lang':true,
@@ -20,7 +21,7 @@ describe('Source code is valid', () => {
       'indent-style':false, //can mix/match
       'indent-width':false, //don't need to beautify
       'line-no-trailing-whitespace': false, //don't need to beautify
-      'id-class-style':false, //I like dashes in classnames
+      'class-style':'none', //I like dashes in classnames
       'img-req-alt':false, //for this test; captured later!
     }
 
@@ -33,22 +34,7 @@ beforeAll(() => {
   $ = cheerio.load(html);
 })
 
-describe('Has appropriate headings', () => {
-  test('Headings are meaningful', () => {
-    expect($('h6').length).toEqual(0); //should not have an <h6>
-    expect($('.time-posted').length).toEqual(3); //should have three .time-posted
-  });
-
-  test('Headings are hierarchical', () => {
-    //don't have implemantation for outline algorithm
-    //just check counts for now
-    expect($('h1').length).toEqual(1); //has 1 <h1>
-    expect($('h2').length).toEqual(3); //has 3 <h2>
-    expect($('h3').length).toEqual(0); //has 0 <h3>
-  });
-})
-
-describe('Include semantic sectioning elements', () => {
+describe('Includes semantic sectioning elements', () => {
   test('Has header, main, and footer sections', () => {
     let bodyChildren = $('body').children();
     expect(bodyChildren.length).toEqual(3);
@@ -58,85 +44,115 @@ describe('Include semantic sectioning elements', () => {
   })
 
   test('Header has appropriate content', () => {
-    expect($('body > header').text().trim()).toMatch(/My Awesome Blog\W+Where I post/);
+    expect($('body > header').text().trim()).toMatch(/My Photo Journal\W+A collection of photographs/);
   })
 
-  test('Has appropriate sections', () => {
-    let mainChildren = $('main').children();
-    expect(mainChildren.length).toEqual(3); //main has three blog posts, three sections
+  test('Main has appropriate sections', () => {
+    let mainSections = $('main').children();
+    expect(mainSections.length).toEqual(2); //main has two sections
 
-    let postTitles = ["Check it out!", "How I did it", "A blog post!"];    
-    mainChildren.each(function(i){
-      //section or article valid, but section is better
-      expect($(this)[0].tagName).toMatch(/(section)|(article)/); //main child is section or article
-
-      //should only contain post title, not titles of other posts
-      let text = $(this).text();
-      expect(text).toMatch(postTitles[i]); //section text contains post title 
-      expect(text).not.toMatch(new RegExp('('+postTitles[(i+1)%3]+'|'+postTitles[(i+2)%3]+')')); //section text doesn't contain other post titles
-    });
+    expect(mainSections.eq(0).text()).toMatch(/The Cutest Puppy/) //has correct content
+    expect(mainSections.eq(0).text()).not.toMatch(/Leave a comment/)
+    expect(mainSections.eq(1).text()).toMatch(/Leave a comment/) //has correct content
+    expect(mainSections.eq(1).text()).not.toMatch(/The Cutest Puppy/)
   })
 
-  test('Blog post times are annotated', () => {
-    //look for <time> tags and make sure they are correct :)
-    expect($('time').length).toEqual(3); //should have three <time> elements
-
-    $('time').each(function(i){
-      let timestamp = $(this).attr('datetime');
-      expect(String(new Date(timestamp))).not.toMatch('Invalid Date'); //`datetime` attribute is valid time
-    })
+  test('First main section has a header', () => {
+    let firstSection = $('main > section').first()
+    let firstSectionChild = firstSection.children().eq(0)
+    expect(firstSectionChild[0].tagName).toMatch('header')
+    expect(firstSectionChild.text()).toMatch(/The Cutest Puppy/)
+    expect(firstSectionChild.text()).toMatch(/Posted/)
+    expect(firstSectionChild.text()).not.toMatch(/OMG I found/)
   })
 })
 
-describe('First post has accessible images', () => {
+describe('Has appropriate headings', () => {
+  test("Headings are hierarchical (don't skip h2)", () => {
+    //don't have implemantation for outline algorithm
+    //just check counts for now
+    expect($('h2').length).toEqual(2); //has 2 <h1>
+    expect($('h3').length).toEqual(0); //has 0 <h3>
+  });
+
+  test("Headings are meaningful (don't use use h6 for styling)", () => {
+    expect($('h6').length).toEqual(0); //should not have an <h6>    
+    expect($('p.time-posted').length).toEqual(1); //should have a paragraph with `time-posted` instead
+  });
+})
+
+describe('Content is annotated for machines/screen-readers', () => {
+  test('Posted time includes a `<date>` element', () => {
+    let timeElement = $('time');
+    expect(timeElement.length).toEqual(1) //has an element
+    let datetime = $(timeElement).attr('datetime')
+    expect(datetime).toEqual('2019-09-01') //has correct date
+  })
+
   test('Image has alternative test', () => {
     let alt = $('img').attr('alt');
     expect(alt).toBeDefined(); //has alt attribute
     expect(alt.length).toBeGreaterThan(3); //alt attribute has content
-    expect(alt).not.toMatch(/an? (image|picture) of/i); //alt attribute doesn't mention image
-  })
-
-  test('Figure is captioned', () => {
-    let figure = $('img').parent('figure');
-    expect(figure.length).toEqual(1); //<img> is inside a <figure>
-    let figcap = figure.children('figcaption');
-    expect(figcap.length).toEqual(1); //<figure> contains a <figcaption>
-    let cite = figcap.children('cite');
-    expect(cite.length).toEqual(1); //<figcaption> contains <cite> tag
-    let citation = cite.html().split('\n').join(' ').replace(/\s{2,}/,' ');
-    let content = new RegExp('Bizzard, the pup, by Frank Harvey. From the <a href="https://www.flickr.com/photos/statelibraryofnsw/2959326615/">State Library of NSW</a>.')
-    expect(citation).toMatch(content); //<cite> contains all the content
+    expect(alt.toLowerCase()).not.toMatch(/an? (image|picture) of/i); //alt attribute doesn't mention image
   })
 })
 
-describe('Second post describes code', () => {
-  test('Abbreviations are annotated', () => {
-    let abbr = $('abbr');
-    expect(abbr.length).toBeGreaterThanOrEqual(2); //has 2 <abbr> elements
-    let abbrHtml = abbr.slice(0,1);
-    expect(abbrHtml.text()).toMatch('HTML'); //first abbr for HTML
-    expect(abbrHtml.attr('title')).toMatch('HyperText Markup Language'); //includes title attribute
-    let abbrCss = abbr.slice(1,2);
-    expect(abbrCss.text()).toMatch('CSS'); //second abbr for CSS
-    expect(abbrCss.attr('title')).toMatch('Cascading Style Sheets'); //includes title attribute
+describe('Uses semantic tags for text', () => {
+  test('Uses <em> elements for emphasis', () => {
+    let emElement = $('em')
+    expect(emElement.length).toBe(1)
+    expect(emElement.text()).toMatch('CUTEST')
+
+    expect($('i').text()).not.toMatch('CUTEST') //not in i
   })
 
-  test('HTML code example is displayed', () => {
-    //one check that it's formatted right
-    expect($('code').html()).toEqual('&lt;h1&gt;Hello World&lt;/h1&gt;'); //contains <code> tag with content
+  test('Uses <cite> elements for citations', () => {
+    let citeElement = $('cite');
+    expect(citeElement.length).toBe(1)
+
+    let citation = citeElement.html().split('\n').join(' ').replace(/\s{2,}/,' ');
+    let content = new RegExp('&quot;Bizzard, the pup&quot;, by Frank Harvey. From the <a href="https://www.flickr.com/photos/statelibraryofnsw/2959326615/">State Library of NSW</a>')
+    expect(citation).toMatch(content); //<cite> contains all the content
+    expect($('i').html().split('\n').join(' ').replace(/\s{2,}/,' ')).not.toMatch(content) //not in i
+
+  })
+})
+
+describe('Form is accessible', () => {
+  test('Inputs have appropriate `type` attributes', () => {
+    expect($('#email_input').attr('type')).toEqual('email')
+    expect($('#password_input').attr('type')).toEqual('password')
   })
 
-  test('CSS components described in descriptive list', () => {
-    let dl = $('dl');
-    expect(dl.length).toEqual(1); //should have a <dl>
-    let tagNames = dl.children().map((i,e) => e.tagName).get().join(' ');
-    expect(tagNames).toMatch('dt dd dt dd'); //should have <dt> and <dd> in order
-    let dt = $('dl > dt'); //need to be immediate children
-    let dd = $('dl > dd');
-    expect(dt.eq(0).html()).toEqual('Selectors'); //has a <dt> for Selectors
-    expect(dt.eq(1).html()).toEqual('Properties'); //has a <dt> for Properties
-    expect(dd.eq(0).html()).toMatch(/Let you say which tags you want to style/i); //
-    expect(dd.eq(1).html()).toMatch(/Let you say what style you want to make those tags/i);    
+  test('Inputs have appropriate `name` attributes', () => {
+    expect($('#email_input').attr('name')).toEqual('email')
+    expect($('#password_input').attr('name')).toEqual('password')
+    expect($('#comment_field').attr('name')).toEqual('comment')
+  })
+
+  test('Inputs have preceding <label> elements', () => {
+    let formLabels = $('form label')
+    expect(formLabels.length).toEqual(3) //3 labels
+
+    let emailLabel = formLabels.eq(0)
+    expect(emailLabel.attr('for')).toEqual('email_input') //has for attribbute
+    expect(emailLabel.html()).toMatch('Email:') //has correct content
+    expect(emailLabel.next().attr('id')).toEqual('email_input') //next element is input#email_input
+
+    let passwordLabel = formLabels.eq(1)
+    expect(passwordLabel.attr('for')).toEqual('password_input') //has for attribbute
+    expect(passwordLabel.html()).toMatch('Password:') //has correct content
+    expect(passwordLabel.next().attr('id')).toEqual('password_input') //next element is input#password_input
+
+    let commentLabel = formLabels.eq(2)
+    expect(commentLabel.attr('for')).toEqual('comment_field') //has for attribbute
+    expect(commentLabel.html()).toMatch('Comment:') //has correct content
+    expect(commentLabel.next().attr('id')).toEqual('comment_field') //next element is textarea#comment_field
+  })
+
+  test('Button icon has an `aria-label', () => {
+    let iconElement = $('button > i');
+    expect(iconElement.attr('aria-label')).toEqual('Submit')
   })
 })
 
@@ -147,7 +163,7 @@ describe('Footer is semantically accurate', () => {
     expect(address.parent('footer').length).toEqual(1); //<address> in <footer>
     expect(address.html()).toMatch(/Contact me at/); //<address> contains info
     expect(address.html()).not.toMatch(/This blog was/); //<address> doesn't include author
-    expect(address.html()).not.toMatch(/2017 The Author./); //<address> doesn't include copyright
+    expect(address.html()).not.toMatch(/2019 The Author./); //<address> doesn't include copyright
     expect(address.html()).not.toMatch(/UW Information School/); //<address> only in footer!
   })
 
@@ -161,5 +177,11 @@ describe('Footer is semantically accurate', () => {
     let tel = $('a[href="tel:555-123-4567"]');
     expect(tel.length).toEqual(1); //has link with telephone formatting
     expect(tel.html()).toEqual('(555) 123-4567'); //telephone link on correct content
+  })
+
+  test('Copyright symbol is an HTML character entity', () => {
+    let copyright = $('footer').children().last()
+    expect(copyright.html()).toMatch(/&#xA9;/) //expected entity
+    expect(copyright.text()).not.toMatch('copyright')
   })
 })
